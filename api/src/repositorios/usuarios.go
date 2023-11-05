64102fb98c7ecc6ac3f0fdf9a390d9bff9622ee8
Rafael Santos <rafa.05125@gmail.com>
2023-11-05 19:17:59 -0300

    Stop following user

--- api/src/repositorios/usuarios.go
+++ api/src/repositorios/usuarios.go
@@ -163,3 +163,19 @@ func (repositorio Usuarios) Seguir(usuarioID, seguidorID uint64) error {
 
 	return nil
 }
+
+func (repositorio Usuarios) PararDeSeguir(usuarioID, seguidorID uint64) error {
+	statement, erro := repositorio.db.Prepare(
+		"DELETE FROM seguidores WHERE usuario_id = ? AND seguidor_id = ?",
+	)
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(usuarioID, seguidorID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
