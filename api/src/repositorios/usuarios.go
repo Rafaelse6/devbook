d91cbaac654673442c10a8415f2f2a8d4cb5e109
Rafael Santos <rafa.05125@gmail.com>
2023-11-05 08:47:35 -0300

    Following user

--- api/src/repositorios/usuarios.go
+++ api/src/repositorios/usuarios.go
@@ -146,3 +146,20 @@ func (repositorio Usuarios) BuscarPorEmail(email string) (modelos.Usuario, error
 	return usuario, nil
 
 }
+
+func (repositorio Usuarios) Seguir(usuarioID, seguidorID uint64) error {
+	statement, erro := repositorio.db.Prepare(
+		"INSERT IGNORE INTO seguidores (usuario_id, seguidor_id) values (?, ?)",
+	)
+	if erro != nil {
+		return erro
+	}
+
+	defer statement.Close()
+
+	if _, erro = statement.Exec(usuarioID, seguidorID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
