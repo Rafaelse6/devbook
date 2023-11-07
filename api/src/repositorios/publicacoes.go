a2fe3c52c89be9a020107da34c2223c744e96ce3
Rafael Santos <rafa.05125@gmail.com>
2023-11-07 14:30:17 -0300

    Finding all posts

--- api/src/repositorios/publicacoes.go
+++ api/src/repositorios/publicacoes.go
@@ -35,6 +35,44 @@ func (repositorio Publicacoes) Criar(publicacao modelos.Publicacao) (uint64, err
 	return uint64(ultimoIDInserido), nil
 }
 
+func (repositorio Publicacoes) Buscar(usuarioID uint64) ([]modelos.Publicacao, error) {
+	linhas, erro := repositorio.db.Query(`
+	SELECT DISTINCT p.*,  u.nick
+	FROM publicacoes p
+	JOIN usuarios u ON p.autor_id = u.id
+	LEFT JOIN seguidores s ON u.id = s.usuario_id
+	WHERE u.id = ? OR s.seguidor_id = ?
+	ORDER BY p.criadaEm DESC;`,
+		usuarioID, usuarioID,
+	)
+	if erro != nil {
+		return nil, erro
+	}
+	defer linhas.Close()
+
+	var publicacoes []modelos.Publicacao
+
+	for linhas.Next() {
+		var publicacao modelos.Publicacao
+
+		if erro = linhas.Scan(
+			&publicacao.ID,
+			&publicacao.Titulo,
+			&publicacao.Conteudo,
+			&publicacao.AutorID,
+			&publicacao.Curtidas,
+			&publicacao.CriadaEm,
+			&publicacao.AutorNick,
+		); erro != nil {
+			return nil, erro
+		}
+
+		publicacoes = append(publicacoes, publicacao)
+	}
+
+	return publicacoes, nil
+}
+
 func (repositorio Publicacoes) BuscarPorID(publicacaoID uint64) (modelos.Publicacao, error) {
 	linha, erro := repositorio.db.Query(`
 	SELECT p.*, u.nick FROM 
