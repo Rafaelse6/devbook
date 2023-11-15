2d2dcc7efa25758932ccc981ebe4dffb200eafd6
Rafael Santos <rafa.05125@gmail.com>
2023-11-14 21:13:54 -0300

    API request

--- webapp/assets/js/cadastro.js
+++ webapp/assets/js/cadastro.js
@@ -2,10 +2,9 @@ $("#formulario-cadastro").on("submit", criarUsuario);
 
 function criarUsuario(evento) {
   evento.preventDefault();
-  console.log("Dentro da função usuário");
 
   if ($("#senha").val() != $("#confirmar-senha").val()) {
-    alert("As senhas não coincidem!");
+    alert("As senhas não são iguais");
     return;
   }
 
@@ -13,10 +12,10 @@ function criarUsuario(evento) {
     url: "/usuarios",
     method: "POST",
     data: {
-      nome: $("nome").val(),
-      email: $("email").val(),
-      nick: $("nick").val(),
-      senha: $("senha").val(),
+      nome: $("#nome").val(),
+      email: $("#email").val(),
+      nick: $("#nick").val(),
+      senha: $("#senha").val(),
     },
   });
 }
